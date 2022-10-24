1fc097b55bcf39cfe75e262dddf040c04c524aae
NanorGosdanian <nanorgosdanian@gmail.com>
2022-10-24 14:38:51 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", (event) =>
+    createNewTask(event, newTaskDescription)
+  );
 });
+
+
+const createNewTask = (event, newTaskDescription) => {
+  event.preventDefault();
+
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+};
+
+const appendNewTask = task => {
+document.getElementById("tasks").appendChild(task);
+};
